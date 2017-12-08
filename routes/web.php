4ef82b2704f7f8f8c532c4226cc402a0f27ca0e9
郑瑞 <455412013@qq.com>
2017-12-08 17:22:56 +0000

    sleep

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);\
+
+//话题分类
+Route::resource('categories',"CategoriesController",['only'=>['show']]);
