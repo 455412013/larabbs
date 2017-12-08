3e7c231709c272ff0925ded4d5d241d4c524c54f
郑瑞 <455412013@qq.com>
2017-12-08 17:25:40 +0800

    Basic user profile editing

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -39,6 +39,9 @@
 
                         <ul class="dropdown-menu" role="menu">
                             <li>
+                                <a href="{{route('users.edit',\Illuminate\Support\Facades\Auth::id())}}">编辑资料</a>
+                            </li>
+                            <li>
                                 <a href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
