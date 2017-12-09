783da2590a8f024b4ea6d6197ae9eca4091759bb
郑瑞 <455412013@qq.com>
2017-12-09 23:50:22 +0800

    　“用户可以创建话题”

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -11,3 +11,9 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+function make_excerpt($value, $length = 200)
+{
+//    strip_tags剥去字符串中的 HTML 标签：
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
